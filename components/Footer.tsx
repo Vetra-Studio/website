91627c33a2ab129a2c19abedf518a23285098cfb
Alien <eliamorari07@gmail.com>
2026-07-30 19:33:05 +0200

    feature: creted Header and Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="text-white p-4">
+      <div className="border-b border-t border-gray-700">
+        <div className="py-4 flex flex-col space-y-4">
+            <h2 className="text-xl font-bold">Vetra Studio</h2>
+            <p className="">Via Roma 123, 00100 Roma (RM)</p>
+            <p className="">Telefono: +39 06 1234567</p>
+        </div>
+      </div>
+      <p className="pt-8" >&copy; 2026 Vetra Studio. All rights reserved.</p>
+    </footer>
+  );
+}
